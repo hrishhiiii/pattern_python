83807a61f3345c7fc5d78cb621e550e1342ffb23
hrishhiiii <163642803+hrishhiiii@users.noreply.github.com>
2024-03-16 15:53:55 +0530

    Add files via upload

---
+++ assignmet.py
@@ -0,0 +1,30 @@
+# for i in range(1,6):
+#     for j in range(i):
+#         print("*",end ="")
+#     print()
+# rows = 4
+# n =1
+# for i in range(1,rows +1):
+#     for j in range(1,i+1):
+#         print(n, end="")
+#         n +=1
+#     print()
+# n=4
+# for i in range(n):
+#     for j in range(i+1):
+#         print(chr(j + 65),end="")
+#     print()
+# n = 65
+# for i in range(1,5):
+#     for j in range(1,i+1):
+#         m = chr(n)
+#         print(m,end="")
+#         n=n+1
+#     print()
+# rows = 5
+# n= 2
+# for i in range(2,rows +1):
+#     for j in range(2,i+1):
+#         print(n, end="")
+#         n +=2
+#     print()
